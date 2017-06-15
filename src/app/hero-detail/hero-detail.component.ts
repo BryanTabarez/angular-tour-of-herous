985439589347723f043a65012b7b766f64656ca0
Bryan Tabarez <bryan.tabarez@correounivalle.edu.co>
2017-06-15 21:04:51 +0000

    6. Routing Revise the HeroDetailComponent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,16 +1,34 @@
-import { Component, Input } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
 
-import { Hero } from '../hero';
+import { Hero }         from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
-
-  constructor() { }
-
+export class HeroDetailComponent implements OnInit {
+  // hero: Hero;
   @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+  
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+  
+  goBack(): void {
+    this.location.back();
+  }
 
 }
